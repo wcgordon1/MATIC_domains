a0750ea33375f6caecb31daf2544d12b319c076d
wcgordon1 <wcgordon1@gmail.com>
2022-10-16 02:25:11 -0700

    mapping, error

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,13 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 
 import "hardhat/console.sol";
 
 contract Domains {
+    mapping(string => address) public domains;
+
+    // Checkout our new mapping! This will store values
+    mapping(string => string) public records;
+
     constructor() {
-        console.log(
-            "THIS IS MY DOMAINS CONTRACT. NICE. READY FOR THE BEST? DOMAIN SERVICE???"
-        );
+        console.log("So Far, this is working, don't f it up...");
+    }
+
+    function register(string calldata name) public {
+        // Check that the name is unregistered (explained in notes)
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // Check that the owner is the transaction sender
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
+        return records[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,22 @@
 const main = async () => {
+    // The first return is the deployer, the second is a random account
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
-};
+    console.log("Contract deployed by:", owner.address);
+
+    let txn = await domainContract.register("doomSHIT");
+    await txn.wait();
+
+    const domainAddress = await domainContract.getAddress("doomSHIT");
+    console.log("Owner of domain doomSHIT:", domainAddress);
+
+    // Trying to set a record that doesn't belong to me!
+    txn = await domainContract.connect(randomPerson).setRecord("doom2", "Haha my domain now!");
+    await txn.wait();
+}
 
 const runMain = async () => {
     try {
